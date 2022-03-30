e83db072bdebac4f97f7c35495ce2fea6c1b5f83
gavinclarke0 <gavinclarke0@gmail.com>
2022-03-29 23:25:52 -0400

    rt

--- FloydMarshallRecursive.cpp
+++ FloydMarshallRecursive.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 #define BASECASE 10
 
-void floydMarshallRecursive(float *A, float *B, float *C, int n ,int vertexCount) {
+float* Matrix2DRandom(int x, int y) {
+    float *matrix = new float[x * y];
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < x; j++) {
+            matrix[(i * x) + j] = (float)(rand());
+        }
+    }
+    return matrix;
+}
 
-    if (n > BASECASE){
+void FloydMarshallRecursive(float *A, float *B, float *C, int n , int vertexCount) {
+
+    if (n > 10){
         float *A11 = A;
         float *A12 = A+(n/2);
         float *A21 = A+(n*vertexCount/2);
@@ -20,30 +31,29 @@ void floydMarshallRecursive(float *A, float *B, float *C, int n ,int vertexCount
         float *C21 = C+(n*vertexCount/2);
         float *C22 = C+(n*vertexCount/2+n/2);
 
-        floydMarshallRecursive(A11, B11, C11, n/2, vertexCount);
-        floydMarshallRecursive(A12, B11, C12, n/2, vertexCount);
-        floydMarshallRecursive(A21, B21, C11, n/2, vertexCount);
-        floydMarshallRecursive(A22, B21, C12, n/2, vertexCount);
-        floydMarshallRecursive(A22, B22, C22, n/2, vertexCount);
-        floydMarshallRecursive(A21, B22, C21, n/2, vertexCount);
-        floydMarshallRecursive(A12, B12, C22, n/2, vertexCount);
-        floydMarshallRecursive(A11, B12, C21, n/2, vertexCount);
+        FloydMarshallRecursive(A11, B11, C11, n/2, vertexCount);
+        FloydMarshallRecursive(A12, B11, C12, n/2, vertexCount);
+        FloydMarshallRecursive(A21, B21, C11, n/2, vertexCount);
+        FloydMarshallRecursive(A22, B21, C12, n/2, vertexCount);
+        FloydMarshallRecursive(A22, B22, C22, n/2, vertexCount);
+        FloydMarshallRecursive(A21, B22, C21, n/2, vertexCount);
+        FloydMarshallRecursive(A12, B12, C22, n/2, vertexCount);
+        FloydMarshallRecursive(A11, B12, C21, n/2, vertexCount);
         return;
     }
 
-    for  (int k = 0; k < vertexCount; k++ ) {
-        for (int i = 0; i < vertexCount; i++ ){
-            for (int j = 0; j < vertexCount; j++ ){
+    for  (int k = 0; k < n; k++ ) {
+        for (int i = 0; i < n; i++ ){
+            for (int j = 0; j < n; j++ ){
                 A[(vertexCount * i) + j] = min(A[(vertexCount * i) + j],   B[(vertexCount * i) + k] +  C[(vertexCount * k) + j]);
             }
         }
     }
 }
 
-
-
-
 int main() {
-    cout << "Hello, World!" << endl;
+
+    float *adjacencyDenseMatrix = Matrix2DRandom(512, 512);
+    FloydMarshallRecursive(adjacencyDenseMatrix, adjacencyDenseMatrix, adjacencyDenseMatrix, 512, 512 );
     return 0;
 }
